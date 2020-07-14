9b3d1dfd349e7cfca62f7b6422748548b8441ae2
Pavel Afanasyev <throttlerhtc@gmail.com>
2020-07-14 23:30:32 +0300

    HW_05_hierarhy

--- src/test/java/ru/javawebinar/topjava/service/MealServiceTest.java
+++ src/test/java/ru/javawebinar/topjava/service/MealServiceTest.java
@@ -1,142 +1,104 @@
 package ru.javawebinar.topjava.service;
 
-import org.junit.AfterClass;
 import org.junit.Assert;
-import org.junit.Rule;
 import org.junit.Test;
-import org.junit.rules.Stopwatch;
-import org.junit.runner.Description;
-import org.junit.runner.RunWith;
-import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.jdbc.Sql;
-import org.springframework.test.context.jdbc.SqlConfig;
-import org.springframework.test.context.junit4.SpringRunner;
-import ru.javawebinar.topjava.ActiveDbProfileResolver;
 import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.util.exception.NotFoundException;
 
 import java.time.LocalDate;
 import java.time.Month;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.concurrent.TimeUnit;
 
 import static org.junit.Assert.assertThrows;
-import static org.slf4j.LoggerFactory.getLogger;
 import static ru.javawebinar.topjava.MealTestData.*;
 import static ru.javawebinar.topjava.UserTestData.ADMIN_ID;
 import static ru.javawebinar.topjava.UserTestData.USER_ID;
 
-@ContextConfiguration({
-        "classpath:spring/spring-app.xml",
-        "classpath:spring/spring-db.xml"
-})
-@RunWith(SpringRunner.class)
-@Sql(scripts = "classpath:db/populateDB.sql", config = @SqlConfig(encoding = "UTF-8"))
-@ActiveProfiles(resolver = ActiveDbProfileResolver.class)
-public class MealServiceTest {
-    private static final Logger log = getLogger("result");
+public class MealServiceTest extends ServiceTest {
 
-    private static final StringBuilder results = new StringBuilder();
+        @Autowired
+        private MealService service;
 
-    private final List<Integer> iter = new ArrayList<>();
 
-    @Rule
-    // http://stackoverflow.com/questions/14892125/what-is-the-best-practice-to-determine-the-execution-time-of-the-bussiness-relev
-    public final Stopwatch stopwatch = new Stopwatch() {
-        @Override
-        protected void finished(long nanos, Description description) {
-            String result = String.format("\n%-25s %7d", description.getMethodName(), TimeUnit.NANOSECONDS.toMillis(nanos));
-            results.append(result);
-            log.info(result + " ms\n");
+        //    @Override
+        @Test
+        public void delete() throws Exception {
+            service.delete(MEAL1_ID, USER_ID);
+            assertThrows(NotFoundException.class, () -> service.get(MEAL1_ID, USER_ID));
         }
-    };
 
-    @Autowired
-    private MealService service;
 
-    @AfterClass
-    public static void printResult() {
-        log.info("\n---------------------------------" +
-                "\nTest                 Duration, ms" +
-                "\n---------------------------------" +
-                results +
-                "\n---------------------------------");
-    }
+        //    @Override
+        @Test
+        public void deleteNotFound() throws Exception {
+            assertThrows(NotFoundException.class, () -> service.delete(NOT_FOUND, USER_ID));
+        }
 
-    @Test
-    public void delete() throws Exception {
-        service.delete(MEAL1_ID, USER_ID);
-        assertThrows(NotFoundException.class, () -> service.get(MEAL1_ID, USER_ID));
-    }
 
-    @Test
-    public void deleteNotFound() throws Exception {
-        assertThrows(NotFoundException.class, () -> service.delete(NOT_FOUND, USER_ID));
-    }
+        @Test
+        public void deleteNotOwn() throws Exception {
+            assertThrows(NotFoundException.class, () -> service.delete(MEAL1_ID, ADMIN_ID));
+        }
 
-    @Test
-    public void deleteNotOwn() throws Exception {
-        assertThrows(NotFoundException.class, () -> service.delete(MEAL1_ID, ADMIN_ID));
-    }
+        //    @Override
+        @Test
+        public void create() throws Exception {
+            Meal created = service.create(getNew(), USER_ID);
+            int newId = created.id();
+            Meal newMeal = getNew();
+            newMeal.setId(newId);
+            MEAL_MATCHER.assertMatch(created, newMeal);
+            MEAL_MATCHER.assertMatch(service.get(newId, USER_ID), newMeal);
+        }
 
-    @Test
-    public void create() throws Exception {
-        Meal created = service.create(getNew(), USER_ID);
-        int newId = created.id();
-        Meal newMeal = getNew();
-        newMeal.setId(newId);
-        MEAL_MATCHER.assertMatch(created, newMeal);
-        MEAL_MATCHER.assertMatch(service.get(newId, USER_ID), newMeal);
-    }
+        //    @Override
+        @Test
+        public void get() throws Exception {
+            Meal actual = service.get(ADMIN_MEAL_ID, ADMIN_ID);
+            MEAL_MATCHER.assertMatch(actual, ADMIN_MEAL1);
+        }
 
-    @Test
-    public void get() throws Exception {
-        Meal actual = service.get(ADMIN_MEAL_ID, ADMIN_ID);
-        MEAL_MATCHER.assertMatch(actual, ADMIN_MEAL1);
-    }
+        //    @Override
+        @Test
+        public void getNotFound() throws Exception {
+            assertThrows(NotFoundException.class, () -> service.get(NOT_FOUND, USER_ID));
+        }
 
-    @Test
-    public void getNotFound() throws Exception {
-        assertThrows(NotFoundException.class, () -> service.get(NOT_FOUND, USER_ID));
-    }
+        @Test
+        public void getNotOwn() throws Exception {
+            assertThrows(NotFoundException.class, () -> service.get(MEAL1_ID, ADMIN_ID));
+        }
 
-    @Test
-    public void getNotOwn() throws Exception {
-        assertThrows(NotFoundException.class, () -> service.get(MEAL1_ID, ADMIN_ID));
-    }
+        //    @Override
+        @Test
+        public void update() throws Exception {
+            Meal updated = getUpdated();
+            service.update(updated, USER_ID);
+            MEAL_MATCHER.assertMatch(service.get(MEAL1_ID, USER_ID), getUpdated());
+        }
 
-    @Test
-    public void update() throws Exception {
-        Meal updated = getUpdated();
-        service.update(updated, USER_ID);
-        MEAL_MATCHER.assertMatch(service.get(MEAL1_ID, USER_ID), getUpdated());
-    }
+        @Test
+        public void updateNotOwn() throws Exception {
+            NotFoundException exception = assertThrows(NotFoundException.class, () -> service.update(MEAL1, ADMIN_ID));
+            Assert.assertEquals("Not found entity with id=" + MEAL1_ID, exception.getMessage());
+        }
 
-    @Test
-    public void updateNotOwn() throws Exception {
-        NotFoundException exception = assertThrows(NotFoundException.class, () -> service.update(MEAL1, ADMIN_ID));
-        Assert.assertEquals("Not found entity with id=" + MEAL1_ID, exception.getMessage());
-    }
+        //    @Override
+        @Test
+        public void getAll() throws Exception {
+            MEAL_MATCHER.assertMatch(service.getAll(USER_ID), MEALS);
+        }
 
-    @Test
-    public void getAll() throws Exception {
-        MEAL_MATCHER.assertMatch(service.getAll(USER_ID), MEALS);
-    }
+        @Test
+        public void getBetweenInclusive() throws Exception {
+            MEAL_MATCHER.assertMatch(service.getBetweenInclusive(
+                    LocalDate.of(2020, Month.JANUARY, 30),
+                    LocalDate.of(2020, Month.JANUARY, 30), USER_ID),
+                    MEAL3, MEAL2, MEAL1);
+        }
 
-    @Test
-    public void getBetweenInclusive() throws Exception {
-        MEAL_MATCHER.assertMatch(service.getBetweenInclusive(
-                LocalDate.of(2020, Month.JANUARY, 30),
-                LocalDate.of(2020, Month.JANUARY, 30), USER_ID),
-                MEAL3, MEAL2, MEAL1);
-    }
-
-    @Test
-    public void getBetweenWithNullDates() throws Exception {
-        MEAL_MATCHER.assertMatch(service.getBetweenInclusive(null, null, USER_ID), MEALS);
-    }
+        @Test
+        public void getBetweenWithNullDates() throws Exception {
+            MEAL_MATCHER.assertMatch(service.getBetweenInclusive(null, null, USER_ID), MEALS);
+        }
 }
--- src/test/java/ru/javawebinar/topjava/service/UserServiceTest.java
+++ src/test/java/ru/javawebinar/topjava/service/UserServiceTest.java
@@ -2,16 +2,9 @@ package ru.javawebinar.topjava.service;
 
 import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.CacheManager;
 import org.springframework.dao.DataAccessException;
-import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.jdbc.Sql;
-import org.springframework.test.context.jdbc.SqlConfig;
-import org.springframework.test.context.junit4.SpringRunner;
-import ru.javawebinar.topjava.ActiveDbProfileResolver;
 import ru.javawebinar.topjava.model.Role;
 import ru.javawebinar.topjava.model.User;
 import ru.javawebinar.topjava.util.exception.NotFoundException;
@@ -21,14 +14,7 @@ import java.util.List;
 import static org.junit.Assert.assertThrows;
 import static ru.javawebinar.topjava.UserTestData.*;
 
-@ContextConfiguration({
-        "classpath:spring/spring-app.xml",
-        "classpath:spring/spring-db.xml"
-})
-@RunWith(SpringRunner.class)
-@Sql(scripts = "classpath:db/populateDB.sql", config = @SqlConfig(encoding = "UTF-8"))
-@ActiveProfiles(resolver = ActiveDbProfileResolver.class)
-public class UserServiceTest {
+public abstract class UserServiceTest extends ServiceTest {
 
     @Autowired
     private UserService service;
@@ -36,11 +22,13 @@ public class UserServiceTest {
     @Autowired
     private CacheManager cacheManager;
 
+
     @Before
     public void setUp() throws Exception {
         cacheManager.getCache("users").clear();
     }
 
+    //    @Override
     @Test
     public void create() throws Exception {
         User created = service.create(getNew());
@@ -57,23 +45,27 @@ public class UserServiceTest {
                 service.create(new User(null, "Duplicate", "user@yandex.ru", "newPass", Role.USER)));
     }
 
+    //    @Override
     @Test
     public void delete() throws Exception {
         service.delete(USER_ID);
         assertThrows(NotFoundException.class, () -> service.get(USER_ID));
     }
 
+    //    @Override
     @Test
-    public void deletedNotFound() throws Exception {
+    public void deleteNotFound() throws Exception {
         assertThrows(NotFoundException.class, () -> service.delete(NOT_FOUND));
     }
 
+    //    @Override
     @Test
     public void get() throws Exception {
         User user = service.get(USER_ID);
         USER_MATCHER.assertMatch(user, USER);
     }
 
+    //    @Override
     @Test
     public void getNotFound() throws Exception {
         assertThrows(NotFoundException.class, () -> service.get(NOT_FOUND));
@@ -85,6 +77,7 @@ public class UserServiceTest {
         USER_MATCHER.assertMatch(user, ADMIN);
     }
 
+    //    @Override
     @Test
     public void update() throws Exception {
         User updated = getUpdated();
@@ -92,6 +85,7 @@ public class UserServiceTest {
         USER_MATCHER.assertMatch(service.get(USER_ID), getUpdated());
     }
 
+    //    @Override
     @Test
     public void getAll() throws Exception {
         List<User> all = service.getAll();
