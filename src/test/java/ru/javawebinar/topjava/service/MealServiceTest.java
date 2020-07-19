201eac29494d423eb5111a0937330f97dac4c382
Pavel Afanasyev <throttlerhtc@gmail.com>
2020-07-19 13:39:24 +0300

    HW_05_hierarchy

--- src/test/java/ru/javawebinar/topjava/service/MealServiceTest.java
+++ src/test/java/ru/javawebinar/topjava/service/MealServiceTest.java
@@ -14,7 +14,7 @@ import static ru.javawebinar.topjava.MealTestData.*;
 import static ru.javawebinar.topjava.UserTestData.ADMIN_ID;
 import static ru.javawebinar.topjava.UserTestData.USER_ID;
 
-public class MealServiceTest extends ServiceTest {
+public abstract class MealServiceTest extends ServiceTest {
 
         @Autowired
         private MealService service;
